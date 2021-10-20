cc1c6331b752cf96de3e1861894eeb8427037328
popokobe <yuki.n3kobe@gmail.com>
2021-10-20 19:43:04 +0900

    scssを使えるようにセットアップ

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,24 +1,29 @@
+import Sass from 'sass'
+import Fiber from 'fibers'
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'src',
+    title: '株式会社タイセイ',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: '株式会社タイセイのHP' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    { src: '~/assets/common.scss' },
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -33,10 +38,21 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: [
+    '@nuxtjs/style-resources',
+  ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    loaders: {
+      scss: {
+        implementation: Sass,
+        sassOptions: {
+          fiber: Fiber
+        }
+      }
+    }
+  },
 
   server: {
     port: 3000,
@@ -48,5 +64,9 @@ export default {
     webpack: {
       poll: true
     }
-  }
+  },
+
+  styleResources: {
+    scss: ['~/assets/_variable.scss'],
+},
 }
