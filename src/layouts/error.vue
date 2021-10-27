db11c452037796edec15d8eed2c0783895062cf5
popokobe <yuki.n3kobe@gmail.com>
2021-10-28 01:43:38 +0900

    エラーページ用レイアウトを作成

---
+++ src/layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="wrapper">
+        <div>
+            <h1>ページが見つかりませんでした。</h1>
+            <p><a href="/">ホーム</a>に戻る</p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .wrapper {
+        height: 400px;
+        text-align: center;
+        display: grid;
+        place-items: center;
+
+        h1 {
+            margin-bottom: 30px;
+        }
+
+        a {
+            text-decoration: underline;
+            color: $clr-main;
+        }
+    }
+</style>
