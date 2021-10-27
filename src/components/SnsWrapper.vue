cee8d8e4d29660633e7c96254ac88d4a50a01e67
popokobe <yuki.n3kobe@gmail.com>
2021-10-28 01:44:18 +0900

    ソーシャルアイコン用のコンポーネント作成

---
+++ src/components/SnsWrapper.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="sns">
+        <SocialIcon v-for="icon in sns" :key=icon.id :icon=icon />
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        sns: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .sns {
+        display: flex;
+    }
+</style>
---
+++ src/components/SocialIcon.vue
@@ -0,0 +1,36 @@
+<template>
+    <li>
+        <a v-if="icon == 'instagram'" class="instagram" target="_blank" href="https://www.instagram.com/taisei.1996131/"></a>
+        <a v-if="icon == 'tiktok'" class="tiktok" target="_blank" href="https://www.tiktok.com/@taisei1996131?"></a>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+    a {
+        --icon-size: 30px;
+        
+        display: block;
+        width: var(--icon-size);
+        height: var(--icon-size);
+
+        &.instagram {
+            background: url('~assets/img/icon-instagram.png') no-repeat;
+            background-size: cover;
+        }
+
+        &.tiktok {
+            background: url('~assets/img/icon-tiktok.png') no-repeat;
+            background-size: cover;
+        }
+    }
+
+</style>
