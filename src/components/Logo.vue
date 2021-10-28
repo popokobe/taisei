b4446ff59cf5c9ccab0eefb9b412a0ba1bacc4e7
popokobe <yuki.n3kobe@gmail.com>
2021-10-29 00:15:34 +0900

    Logoコンポーネントに今までのロゴの写真を追加

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,30 +1,58 @@
 <template>
     <div class="logo">
         <a href="/">
-            <span class="sub">株式会社タイセイ</span>
-            <span class="main">TAISEI GROUP</span>
+            <img src="~/assets/img/logo.jpg">
+            <div class="text">
+                <span class="sub">株式会社タイセイ</span>
+                <span class="main">TAISEI GROUP</span>
+            </div>
         </a>
     </div>
 </template>
 
 <style lang="scss" scoped>
+    .logo {
+        color: $clr-main;
 
-.logo {
-    color: $clr-main;
+        a {
+            display: flex;
+            align-items: center;
 
-    span {
-        display: block;
-        letter-spacing: $ls-md;
-        line-height: $lh-125;
+            img {
+                --icon-size: 30px;
+                
+                @include mq(tab) {
+                    --icon-size: 45px;
+                }
 
-        &.sub {
-            font-size: 12px;
-        }
+                width: var(--icon-size);
+                height: var(--icon-size);
+                margin-right: 5px;
+            }
 
-        &.main {
-            font-size: 24px;
+            .text {
+                span {
+                    display: block;
+                    letter-spacing: $ls-md;
+                    line-height: $lh-125;
+
+                    &.sub {
+                        font-size: 8px;
+
+                        @include mq(tab) {
+                            font-size: 12px;
+                        }  
+                    }
+
+                    &.main {
+                        font-size: 14px;
+                        
+                        @include mq(tab) {
+                            font-size: 24px;
+                        }
+                    }
+                }
+            }
         }
     }
-}
-
 </style>
